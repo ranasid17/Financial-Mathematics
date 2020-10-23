dc8c45419305e55d46f4f339ab0cdd9d02900240
Sid <39922327+ranasid17@users.noreply.github.com>
2020-10-22 19:37:20 -0500

    Update visTwoDayChange.py

--- visTwoDayChange.py
+++ visTwoDayChange.py
@@ -1,16 +1,32 @@
-import matplotlib.pyplot as plt 
-import seaborn as sns
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 18:31:20 2020
 
-# Goal: Create and visualize PDF of twoDayChanges.py 
-#   1) Must run twoDayChanges.py prior to this to create vars
+@author: Sid
+"""
+import matplotlib.pyplot as plt
+import numpy as np 
+from matplotlib import colors
 
-# Run functions for twoDayChanges.py
-a, b = getPrices(openPrices, closePrices)
-c = relativeChange(a, b) 
-
-# create PDF distribution
-sns.distplot(c, hist=True, kde=True, bins=50, hist_kws={'edgecolor':'black'},kde_kws={'linewidth': 1})
-# modify plotting 
-plt.ylabel('Observed Probability of Given Rel. Price Change') 
-plt.xlabel('Observed 48h Relative Price Changes')
-plt.title('48h Relative Price Changes of SPY, Oct 2019 - 2020')
+def plotHistogram(arr): 
+    # actual histogram command
+    n, bins, patches = plt.hist(arr, bins = 20, density = True) 
+    # ncolor code by height (AKA: likelihood)
+    fracs = n / arr.max()
+    # normalize fracts from [0,1]
+    norm = colors.Normalize(fracs.min(), fracs.max())
+    # loop thru each bin and set colors (using viridis map)
+    for thisfrac, thispatch in zip(fracs, patches):
+        color = plt.cm.viridis(norm(thisfrac))
+        thispatch.set_facecolor(color)
+    
+    # set x ticks to be +/- 1 from input min/max
+    plt.xticks(np.arange(min(arr), max(arr), 2.0))
+    # x, y, and plot titles
+    plt.xlabel('Relative Percent Price Change')
+    plt.ylabel('Observed Probability')
+    plt.title('48h Relative Price Change of SPY Oct 2019 - Oct 2020')
+    # display plot 
+    plt.show 
+    
