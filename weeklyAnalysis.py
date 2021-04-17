ecbf14be19f99f0b6a33a4ad48d76a724b226ccb
Sid <39922327+ranasid17@users.noreply.github.com>
2021-04-17 01:25:28 -0500

    Added commands to produce SPY_comparison plot

--- weeklyAnalysis.py
+++ weeklyAnalysis.py
@@ -6,32 +6,43 @@ Created on Fri Apr  9 19:46:35 2021
 @author: Sid
 """
 
-import dataAnalysis as data
+import tradingAnalysisMethods as tam
     
-def main(): 
+if __name__ == "__main__":
+    
     """ 1) Load and initialize data"""
     # import weekly trading data as data frame object 
-    df = data.preprocessing.clean_df() 
+    df = tam.preprocessing.clean_df() 
     # extract trading weeks 
-    weeks = data.preprocessing.extract_trading_weeks(df) 
-    # get list of YTD mondays, fridays to define start/stop of each trading wk 
-    YTD_mondays, YTD_fridays = data.preprocessing.pull_weekdays(2021, weeks)
-    
+    weeks = tam.preprocessing.extract_trading_weeks(df) 
+
     """ 2) Analyze weekly trading returns"""
     # collect weekly returns, cumulative returns, and adjusted for fees
-    rev, cum_rev, adj_cum_rev = data.trading_analysis.revenue(df, weeks)
+    rev, cum_rev, adj_cum_rev = tam.trading_analysis.revenue(df, weeks)
     # repeat above for weekly, cumulative, and adjusted ROI (percentage)
-    ROI, cum_ROI, adj_cum_ROI = data.trading_analysis.revenue(df, weeks)
+    ROI, cum_ROI, adj_cum_ROI = tam.trading_analysis.ROI(df, weeks)
     # collect weekly balance (amt money available for trades)
-    balance = data.trading_analysis.revenue(df, weeks)
+    balance = tam.trading_analysis.balance(df, weeks)
     
     """ 3) Compare to benchmark: SPY """
     # collect daily open and close prices for SPY 
-    spy_opens, spy_closes = data.spy_analysis.extract_prices("SPY")
+    spy_opens, spy_closes = tam.spy_analysis.extract_prices("SPY")
+    # get list of YTD mondays, fridays to define start/stop of each trading wk 
+    YTD_mondays, YTD_fridays = tam.preprocessing.pull_weekdays(2021, weeks)
     # truncate spy_opens, spy_closes to just Monday, Friday prices
-    spy_opens, spy_close = data.spy_analysis.get_monday_friday_prices(
+    spy_opens, spy_closes = tam.spy_analysis.get_monday_friday_prices(
         spy_opens, spy_closes, YTD_mondays, YTD_fridays) 
+    # calculate weekly and cumulative SPY returns and ROI
+    rev_spy, cum_rev_spy, ROI_spy, cum_ROI_spy = tam.spy_analysis.weekly_value(
+        spy_opens, spy_closes, weeks, YTD_mondays, YTD_fridays)
     
-if __name__ == "__main__":
-    main()
+    
+    """ 4) Plot results"""
+    # plot weekly and cumulative returns 
+    tam.plots.weekly_values(weeks, rev, cum_rev)
+    # plot weekly and cumulative ROI 
+    tam.plots.weekly_values(weeks, ROI, cum_ROI)
+    # plot weekly and cumulative fund ROI vs SPY ROI 
+    tam.plots.spy_comparison(weeks, ROI, cum_ROI, ROI_spy, cum_ROI_spy)
+
 
